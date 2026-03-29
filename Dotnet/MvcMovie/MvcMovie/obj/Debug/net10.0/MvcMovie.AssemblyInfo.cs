f2190eba30bdfb20cfd78bf5aea1d0b07a7f48e8
Vatorczek <vatorczek13@gmail.com>
2026-03-29 11:06:14 +0200

    net 2

--- Dotnet/MvcMovie/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Dotnet/MvcMovie/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933b6f97feffe5196b40ed5d8fca940d7b2a8734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d8dd538d77d90d53b1254a0fc5d2ac0548020d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

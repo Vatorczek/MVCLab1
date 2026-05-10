bcc8aad1a27352d0010c31de03524a82ece2c8f3
Vatorczek <vatorczek13@gmail.com>
2026-05-10 10:17:37 +0200

    v0

---
+++ Projekt/Portfelik/Portfelik/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Portfelik.Models;
+using System.Diagnostics;
+
+namespace Portfelik.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
